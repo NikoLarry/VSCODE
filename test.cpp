1ee3c1fbe93c2807a0b89540ad2f8c738bfa1eaf
NikoLarry <1826517606@qq.com>
2024-05-28 17:21:47 +0800

    1

--- test.cpp
+++ test.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 using namespace std;
 
+int forLoop(int n) {
+    int res = 0;
+    // 循环求和 1, 2, ..., n-1, n
+    for (int i = 1; i <= n; i++) {
+        res += i;
+    }
+    return res;
+}
+
+
 int main()
 {
-    cout<<"heelo";
+    int a,b;
+    a=8092;
+    b=32;
+    cout<<a%b;
     return 0 ;
 }
